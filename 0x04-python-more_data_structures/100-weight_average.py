1fdcaa8f4d6f93dee79e693b223aa4edd9d02bed
Watiri-M <watirimungai3737@gmail.com>
2022-12-07 03:07:08 -0800

    Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,15 @@
+
 #!/usr/bin/python3
 
+
 def weight_average(my_list=[]):
-    if my_list is not None:
-        weighted_numerator = 0
-        weight_sum = 0
-        for score, weight in my_list:
-            weighted_numerator += (score * weight)
-            weight_sum += weight
-        average = (weighted_numerator / weight_sum)
-        return average
+    """Return the weighted average of all integers in a list of tuples."""
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
+
+    avg = 0
+    size = 0
+    for tup in my_list:
+        avg += (tup[0] * tup[1])
+        size += tup[1]
+    return (avg / size)
