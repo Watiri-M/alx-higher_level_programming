39b964f63e838e8a58e5f4f1ea9f082d340aec27
Watiri-M <watirimungai3737@gmail.com>
2023-01-10 03:11:48 -0800

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-
-"""File Json Module."""
+"""
+Module content
+Add all arguments to a Python list, and then save to a file
+"""
+import os.path
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item():
-    """Add all arguments to a Python list, and then save them to a file."""
-    _len = len(argv)
-    mylist = []
-    try:
-        mylist = load_from_json_file("add_item.json")
-    except Exception:
-        save_to_json_file(mylist, "add_item.json")
-    if _len < 2:
-        return
-    for i in range(1, _len):
-        mylist.append(argv[i])
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-    save_to_json_file(mylist, "add_item.json")
+obj = load_from_json_file("add_item.json") if os.path.exists(
+    "add_item.json") else []
 
-
-add_item()
+obj.extend(argv[1:])
+save_to_json_file(obj, "add_item.json")
