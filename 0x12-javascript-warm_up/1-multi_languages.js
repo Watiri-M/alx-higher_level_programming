1cdf123c43e5f59917da74d89608ba1e2b28c723
Watiri-M <watirimungai3737@gmail.com>
2023-03-13 05:19:15 -0700

    t1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-let l1 = 'C is fun';
-let l2 = 'Python is cool';
-let l3 = 'JavaScript is amazing';
-console.log(l1);
-console.log(l2);
-console.log(l3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
